069a31e970c2ccb4e8cf5dc75e4b86fc3107af77
izmailovoybek <135944537+izmailovoybek@users.noreply.github.com>
2025-10-10 13:36:59 +0500

    Update homework.sql

--- lesson-7/homework/homework.sql
+++ lesson-7/homework/homework.sql
@@ -1,1 +1,17 @@
-l
+#Write a query to find the minimum (MIN) price of a product in the Products table.
+select min(price) as min_price
+from Products
+#Write a query to find the maximum (MAX) Salary from the Employees table.
+select max(Salary) as max_salary
+from Employees
+#Write a query to count the number of rows in the Customers table.
+select count(*) as count_Customers
+from Customers
+#Write a query to count the number of unique product categories from the Products table.
+SELECT COUNT(DISTINCT Category) AS Unique_Categories
+FROM Products
+#Write a query to find the total sales amount for the product with id 7 in the Sales table.
+SELECT SUM(SaleAmount) AS Total_Sales_Amount
+FROM Sales
+WHERE ProductID = 7
+#
