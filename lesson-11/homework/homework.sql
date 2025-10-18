06e285cdaf955f1f6efd258abd22dac4337fed20
izmailovoybek <135944537+izmailovoybek@users.noreply.github.com>
2025-10-18 12:03:47 +0500

    Update homework.sql

--- lesson-11/homework/homework.sql
+++ lesson-11/homework/homework.sql
@@ -1,1 +1,134 @@
-
+#Return: OrderID, CustomerName, OrderDate
+Task: Show all orders placed after 2022 along with the names of the customers who placed them.
+Tables Used: Orders, Customers
+SELECT 
+    o.OrderID,
+    CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName,
+    o.OrderDate
+FROM Orders o
+JOIN Customers c 
+    ON o.CustomerID = c.CustomerID
+WHERE YEAR(o.OrderDate) > 2022;
+#Return: EmployeeName, DepartmentName
+Task: Display the names of employees who work in either the Sales or Marketing department.
+Tables Used: Employees, Departments
+SELECT 
+    e.EmployeeName,
+    d.DepartmentName
+FROM Employees e
+JOIN Departments d 
+    ON e.DepartmentID = d.DepartmentID
+WHERE d.DepartmentName IN ('Sales', 'Marketing');
+#Return: DepartmentName, MaxSalary
+Task: Show the highest salary for each department.
+Tables Used: Departments, Employees
+SELECT 
+    d.DepartmentName,
+    MAX(e.Salary) AS MaxSalary
+FROM Employees e
+JOIN Departments d 
+    ON e.DepartmentID = d.DepartmentID
+GROUP BY d.DepartmentName;
+#Return: CustomerName, OrderID, OrderDate
+Task: List all customers from the USA who placed orders in the year 2023.
+Tables Used: Customers, Orders
+SELECT 
+    CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName,
+    o.OrderID,
+    o.OrderDate
+FROM Customers c
+JOIN Orders o 
+    ON c.CustomerID = o.CustomerID
+WHERE c.Country = 'USA'
+  AND YEAR(o.OrderDate) = 2023;
+#Return: CustomerName, TotalOrders
+Task: Show how many orders each customer has placed.
+Tables Used: Orders , Customers
+SELECT 
+    CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName,
+    COUNT(o.OrderID) AS TotalOrders
+FROM Customers c
+LEFT JOIN Orders o 
+    ON c.CustomerID = o.CustomerID
+GROUP BY c.FirstName, c.LastName;
+#Return: ProductName, SupplierName
+Task: Display the names of products that are supplied by either Gadget Supplies or Clothing Mart.
+Tables Used: Products, Suppliers
+SELECT 
+    p.ProductName,
+    s.SupplierName
+FROM Products p
+JOIN Suppliers s 
+    ON p.SupplierID = s.SupplierID
+WHERE s.SupplierName IN ('Gadget Supplies', 'Clothing Mart');
+#Return: CustomerName, MostRecentOrderDate
+Task: For each customer, show their most recent order. Include customers who haven't placed any orders.
+Tables Used: Customers, Orders
+SELECT 
+    CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName,
+    MAX(o.OrderDate) AS MostRecentOrderDate
+FROM Customers c
+LEFT JOIN Orders o 
+    ON c.CustomerID = o.CustomerID
+GROUP BY c.FirstName, c.LastName;
+#Return: CustomerName, OrderTotal
+Task: Show the customers who have placed an order where the total amount is greater than 500.
+Tables Used: Orders, Customers
+Quyidagi SQL so‘rov buyurtma summasi 500 dan katta bo‘lgan mijozlarni va ularning buyurtma summasini
+  SELECT 
+    CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName,
+    o.TotalAmount AS OrderTotal
+FROM Orders o
+JOIN Customers c 
+    ON o.CustomerID = c.CustomerID
+WHERE o.TotalAmount > 500;
+#Return: ProductName, SaleDate, SaleAmount
+Task: List product sales where the sale was made in 2022 or the sale amount exceeded 400.
+Tables Used: Products, Sales
+  Quyidagi SQL so‘rov 2022-yilda sotilgan yoki sotuv summasi 400 dan katta bo‘lgan mahsulotlarni, ularning sotuvi sanasi va summasi bilan birga ko‘rsatadi
+  SELECT 
+    p.ProductName,
+    s.SaleDate,
+    s.SaleAmount
+FROM Sales s
+JOIN Products p 
+    ON s.ProductID = p.ProductID
+WHERE YEAR(s.SaleDate) = 2022
+   OR s.SaleAmount > 400;
+#Return: ProductName, TotalSalesAmount
+Task: Display each product along with the total amount it has been sold for.
+Tables Used: Sales, Products
+Quyidagi SQL so‘rov har bir mahsulot bo‘yicha umumiy sotuv summasini ko‘rsatadi
+SELECT 
+    p.ProductName,
+    SUM(s.SaleAmount) AS TotalSalesAmount
+FROM Sales s
+JOIN Products p 
+    ON s.ProductID = p.ProductID
+GROUP BY p.ProductName;
+#Return: EmployeeName, DepartmentName, Salary
+Task: Show the employees who work in the HR department and earn a salary greater than 60000.
+Tables Used: Employees, Departments
+Quyidagi SQL so‘rov HR bo‘limida ishlaydigan va oyligi 60000 dan katta bo‘lgan xodimlarni ularning bo‘lim nomi bilan birga ko‘rsatadi
+  SELECT 
+    e.EmployeeName,
+    d.DepartmentName,
+    e.Salary
+FROM Employees e
+JOIN Departments d 
+    ON e.DepartmentID = d.DepartmentID
+WHERE d.DepartmentName = 'HR'
+  AND e.Salary > 60000;
+  #Return: ProductName, SaleDate, StockQuantity
+Task: List the products that were sold in 2023 and had more than 100 units in stock at the time.
+Tables Used: Products, Sales
+  Quyidagi SQL so‘rov 2023-yilda sotilgan va omborda sotilish vaqtida 100 dan ortiq birlik mavjud bo‘lgan mahsulotlarni ko‘rsatadi
+  SELECT 
+    p.ProductName,
+    s.SaleDate,
+    s.StockQuantity
+FROM Sales s
+JOIN Products p 
+    ON s.ProductID = p.ProductID
+WHERE YEAR(s.SaleDate) = 2023
+  AND s.StockQuantity > 100;
